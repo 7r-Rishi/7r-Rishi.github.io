4bea77741a9edbb1e86603e475654474572e1f7e
7r-Rishi <7r.rushi@gmail.com>
2023-03-16 14:41:12 +0530

    Portfolio working started

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// home typing
+var typed = new Typed(".typing", {
+    strings: ["Backed Developer", "Trader" , "Footballer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+// about me typing
+var typed = new Typed(".typing-2", {
+    strings: ["Backed Developer", "Trader" , "Footballer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
+
+
+
+// Tech Stack append
+
+
+
+
+
+
