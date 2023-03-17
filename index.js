be84717a464255ffdc8e4b3f1ed174965c57a39b
7r-Rishi <7r.rushi@gmail.com>
2023-03-17 14:19:13 +0530

    almost completed

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ var typed = new Typed(".typing-2", {
 });
 
 
+// Github calender
+GitHubCalendar(".calendar", "7r-Rishi");
 
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "7r-Rishi", { responsive: true });
 
-
-
-// Tech Stack append
-
-
-
-
-
-
+// Use a proxy
+GitHubCalendar(".calendar", "7r-Rishi", {
+   proxy (username) {
+     return fetch(`https://your-proxy.com/github?user=$7r-Rishi`)
+   }
+}).then(r => r.text())
